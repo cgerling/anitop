b52bac27dada36020ac9053fce92e8d551db5f32
Calvin Gerling <calvingerling@outlook.com>
2017-11-15 20:15:09 -0200

    Add PDO and Adapter of Watchitem

---
+++ server/src/adapter/watchitem-adapter.php
@@ -0,0 +1,65 @@
+<?php
+require_once "adapter/iadapter.php";
+require_once "adapter/anime-adapter.php";
+require_once "adapter/user-adapter.php";
+require_once "adapter/status-adapter";
+require_once "entities/watchitem.php";
+
+class WatchitemAdapter implements iAdapter {
+    private $animeAdapter;
+    private $userAdapter;
+    private $statusAdapter;
+
+    public function __construct() {
+        $this->animeAdapter = new AnimeAdapter();
+        $this->userAdapter = new UserAdapter();
+        $this->statusAdapter = new StatusAdapter();
+    }
+
+    /**
+     * Adapt a Database ResultSet to an Entity instance
+     * @param $resultset database result set
+     * @return Entity
+     */
+    public function toEntity($resultset)
+    {
+        $anime = $this->animeAdapter->toEntity($resultset);
+        $user = $this->userAdapter->toEntity($resultset);
+        $status = $this->statusAdapter->toEntity($resultset);
+
+        $watchitem = new Watchitem($anime, $user, $status);
+
+        return $watchitem;
+    }
+
+    /**
+     * Adapt a Database ResultSet to an Entity array instance
+     * @param $resultset
+     * @return array
+     */
+    public function toEntityArray($resultset)
+    {
+        $watchlist = array();
+
+        foreach ($resultset as $watchitem) {
+            $watchlist[] = $this->toEntity($watchitem);
+        }
+
+        return $watchlist;
+    }
+
+    /**
+     * @param $anime
+     * @return array
+     */
+    public function toMap($entity): array
+    {
+        $watchitemMap = array();
+
+        foreach ($entity as $key=>$value) {
+            $watchitemMap[$key] = $value->id;
+        }
+
+        return $watchitemMap;
+    }
+}
---
+++ server/src/pdo/watchitem-pdo.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once "query-manager.php";
+require_once "entities/watchitem.php";
+require_once "adapter/watchitem-adapter.php";
+
+class WatchitemPDO {
+    private $table;
+    private $query;
+    private $adapter;
+
+    private $tableJoin = array(
+        0 => array(
+            "targetTable" => "anime",
+            "targetColumn" => "animeid",
+            "sourceColumn" => "anime"
+        ),
+        1 => array(
+            "targetTable" => "status",
+            "targetColumn" => "statusid",
+            "sourceColumn" => "status"
+        )
+    );
+
+    public function __construct() {
+        $this->table = "watchlist";
+        $this->query = new QueryManager($this->table);
+        $this->adapter = new WatchitemAdapter();
+    }
+
+    public function selectById(int $id) {
+        $filter = array(
+            0 => "watchlist.wathclistid = {$id}"
+        );
+
+        $resultset = $this->query->selectOne($this->tableJoin, $filter);
+        $watchitem = $this->adapter->toEntity($resultset);
+
+        return $watchitem;
+    }
+
+    public function selectByUser(int $userId) {
+        $filter = array(
+            0 => "watchlist.user = {$userId}"
+        );
+
+        $resultset = $this->query->select($this->tableJoin, $filter);
+        $watchlist = $this->adapter->toEntityArray($resultset);
+
+        return $watchlist;
+    }
+
+    public function selectAll() {
+        $resultset = $this->query->selectAll($this->tableJoin);
+        $watchlist = $this->adapter->toEntityArray($resultset);
+
+        return $watchlist;
+    }
+
+    public function create(Watchitem $watchitem) {
+        $values = $this->adapter->toMap($watchitem);
+
+        unset($values["watchlistid"]);
+
+        $this->query->insert($values);
+    }
+
+    public function update(Watchitem $watchitem) {
+        $fields = $this->adapter->toMap($watchitem);
+        $filter = array(
+            0 => "watchlist.watchlistid = {$watchitem->id}"
+        );
+
+        $this->query->update($fields, $filter);
+    }
+
+    public function delete(Watchitem $watchitem) {
+        $filter = array(
+            0 => "watchlist.watchlistid = {$watchitem->id}"
+        );
+
+        $this->query->delete($filter);
+    }
+}
