8c2a8a841cd850a0ac591a3c7bcf6dcc94fe1c28
Calvin Gerling <calvingerling@outlook.com>
2017-11-15 16:51:24 -0200

    Add a interface adapter

---
+++ server/src/adapter/iadapter.php
@@ -0,0 +1,25 @@
+<?php
+
+interface iAdapter {
+
+    /**
+     * Adapt a Database ResultSet to an Entity instance
+     * @param $resultset database result set
+     * @return Entity
+     */
+    public function toEntity($resultset);
+
+
+    /**
+     * Adapt a Database ResultSet to an Entity array instance
+     * @param $resultset
+     * @return array
+     */
+    public function toEntityArray($resultset);
+
+    /**
+     * @param $anime
+     * @return array
+     */
+    public function toMap($anime): array;
+}
