67add64dbec4d60a336859729b5bf8ed382c0b31
Calvin Gerling <calvingerling@outlook.com>
2017-11-15 16:50:48 -0200

    Make the fields public and remove the get and set

--- server/src/entities/anime.php
+++ server/src/entities/anime.php
@@ -2,12 +2,12 @@
 require_once "entity.php";
 
 class Anime extends Entity {
-    private $name;
-    private $seasonRelease;
-    private $description;
-    private $author;
-    private $publisher;
-    private $genres;
+    public $name;
+    public $seasonRelease;
+    public $description;
+    public $author;
+    public $publisher;
+    public $genres;
 
     public function __construct(string $name, DateTime $seasonRelease, string $description, string $author, string $publisher, array $genres) {
         $this->name = $name;
@@ -16,47 +16,5 @@ class Anime extends Entity {
         $this->author = $author;
         $this->publisher = $publisher;
         $this->genres = $genres;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName(): string {
-        return $this->name;
-    }
-
-    /**
-     * @return DateTime
-     */
-    public function getSeasonRelease(): DateTime {
-        return $this->seasonRelease;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDescription(): string {
-        return $this->description;
-    }
-
-    /**
-     * @return string
-     */
-    public function getAuthor(): string {
-        return $this->author;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPublisher(): string {
-        return $this->publisher;
-    }
-
-    /**
-     * @return array
-     */
-    public function getGenres(): array {
-        return $this->genres;
     }
 }
--- server/src/entities/entity.php
+++ server/src/entities/entity.php
@@ -1,18 +1,4 @@
 <?php
 abstract class Entity {
-    protected $id;
-
-    /**
-     * @param int $id
-     */
-    public function setId(int $id) {
-        $this->id = $id;
-    }
-
-    /**
-     * @return int
-     */
-    public function getId(): int {
-        return $this->id;
-    }
+    public $id;
 }
