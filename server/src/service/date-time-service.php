27123962853aaf43bacfc1687b0753be7affec2c
Calvin Gerling <calvingerling@outlook.com>
2017-11-15 16:51:40 -0200

    Create a DateTimeService

---
+++ server/src/service/date-time-service.php
@@ -0,0 +1,11 @@
+<?php
+
+class DateTimeService {
+    public static function fromString($date, $format = "Y-m-d") {
+        return DateTime::createFromFormat($format, $date);
+    }
+
+    public static function toString($date, $format = "Y-m-d") {
+        return $date->format($format);
+    }
+}
