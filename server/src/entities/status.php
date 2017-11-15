08aee44c4b4d0352368a8d9fbe5fe693e7cfdc1f
Calvin Gerling <calvingerling@outlook.com>
2017-11-15 17:38:45 -0200

    Make all the fields public

--- server/src/entities/status.php
+++ server/src/entities/status.php
@@ -2,26 +2,11 @@
 require_once "status.php";
 
 class Status extends Entity {
-    
-    private $name;
-    private $description;
+    public $name;
+    public $description;
 
     public function __construct(string $name, string $description) {
         $this->name = $name;
         $this->description = $description;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName(): string {
-        return $this->name;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDescription(): string {
-        return $this->description;
     }
 }
