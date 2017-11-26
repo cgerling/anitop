11d35e7a1f9e03350c66cbd52c3e338afc874449
Calvin Gerling <calvingerling@outlook.com>
2017-11-26 16:40:17 -0200

    Add AnimeWatch component
    Button to watch or remove the anime from watchlist

---
+++ app/src/components/AnimeWatch.vue
@@ -0,0 +1,40 @@
+<template>
+  <button :class="{ 'watching': watching, 'not-watching': !watching }">
+    <icon :name="iconName" />
+    {{text}}
+  </button>
+</template>
+<script>
+import Icon from './Icon'
+
+export default {
+  name: 'AnimeWatch',
+  components: { Icon },
+  props: ['watching'],
+  computed: {
+    iconName () {
+      return this.watching ? 'minus' : 'plus'
+    },
+    text() {
+      return this.watching ? 'Remover' : 'Assistir'
+    }
+  }
+}
+</script>
+<style scoped>
+button {
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  outline: none;
+}
+
+button.watching {
+  background: #ff3860;
+}
+
+button.not-watching {
+  background: #23d160;
+}
+</style>
