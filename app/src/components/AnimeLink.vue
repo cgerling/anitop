b7b8409e28a3aa86ac1961556f553d76940ac4b8
Calvin Gerling <calvingerling@outlook.com>
2017-11-26 16:27:27 -0200

    Add AnimeLink component
    Component to show the AnimePoster and redirect to details page when clicked

---
+++ app/src/components/AnimeLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-link :to="path" tag="div" class="pointer">
+    <anime-poster :name="name" :url="url" />
+  </router-link>
+</template>
+<script>
+import AnimePoster from './AnimePoster'
+
+export default {
+  name: 'AnimeLink',
+  components: { AnimePoster },
+  props: ['id', 'name', 'url'],
+  computed: {
+    path () {
+      return `/anime/${this.div}`
+    }
+  }
+}
+</script>
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+</style>
