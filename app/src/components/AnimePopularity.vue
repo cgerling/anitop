49685988fd654c8ce89c105d03902128802bc603
Calvin Gerling <calvingerling@outlook.com>
2017-11-26 16:34:14 -0200

    Add AnimePopularity component
    Component shows the quantity of people watching the anime

---
+++ app/src/components/AnimePopularity.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="popularity">
+    <div class="quantity">
+      <icon name="television" />
+      <p>
+        {{quantity}}
+      </p>
+    </div>
+    <slot name="button" />
+  </div>
+</template>
+<script>
+import Icon from './Icon'
+
+export default {
+  name: 'AnimePopularity',
+  components: { Icon },
+  props: ['quantity']
+}
+</script>
+<style scoped>
+.popularity {
+  align-items: stretch;
+  background: #209cee;
+  color: #fff;
+  display: flex;
+  font-weight: bold;
+  justify-content: space-between;
+  padding-left: 10%;
+  position: relative;
+  text-align: center;
+  width: 100%;
+}
+
+.quantity {
+  flex-grow: 1;
+}
+</style>
