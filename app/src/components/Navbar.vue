13728b6bb3bc8af07746ccddc3266130a98be9ee
Calvin Gerling <calvingerling@outlook.com>
2017-11-26 18:38:31 -0200

    Add Search component in Navbar

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -6,23 +6,39 @@
       </router-link>
     </div>
     <div class="options" v-if="logged">
-      <icon name="search" />
+      <button @click="toggleSearch">
+        <icon name="search" />
+      </button>
       <button @click="logout">
         <icon name="sign-out" />
       </button>
     </div>
+    <search class="float-bottom" :class="{ 'open': showSearch, 'closed': !showSearch }" :show="showSearch" @search="search" @close="toggleSearch" />
   </nav>
 </template>
 <script>
 import Icon from './Icon'
+import Search from './Search'
 
 export default {
   name: 'Navbar',
-  components: { Icon },
+  components: { Icon, Search },
   props: ['logged'],
+  data () {
+    return {
+      showSearch: false
+    }
+  },
   methods: {
+    search (term) {
+      // TODO: implement search
+      console.info(term)
+    },
     logout () {
       this.$emit('logout')
+    },
+    toggleSearch () {
+      this.showSearch = !this.showSearch
     }
   }
 }
@@ -62,7 +78,26 @@ nav {
 button {
   background: transparent;
   border: none;
+  color: #4a4a4a;
   cursor: pointer;
   outline: none;
+}
+
+.float-bottom {
+  box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.1);
+  position: absolute;
+  left: 0;
+  right: 0;
+  transition: ease all 320ms;
+  z-index: -1;
+}
+
+.open {
+  bottom: -26px;
+}
+
+.closed {
+  bottom: 0;
+  visibility: hidden;
 }
 </style>
