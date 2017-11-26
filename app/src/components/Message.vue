9a423ed70b18407be5ca8e1b5630d9c3d82dc053
Calvin Gerling <calvingerling@outlook.com>
2017-11-26 18:39:19 -0200

    Add Message component

---
+++ app/src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="notification">
+    <p>{{text}}</p>
+  </div>
+</template>
+<script>
+import Icon from './Icon'
+
+export default {
+  name: 'Message',
+  components: { Icon },
+  props: ['text']
+}
+</script>
+<style scoped>
+.notification {
+  margin: 1.25em auto;
+  width: 80%;
+}
+</style>
