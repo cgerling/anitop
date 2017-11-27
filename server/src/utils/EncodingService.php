b2825ed0724d56d84187da4095e7770a89830950
Calvin Gerling <calvingerling@outlook.com>
2017-11-27 21:09:47 -0200

    Add encoding service to convert utf8 string to a compatible format

---
+++ server/src/utils/EncodingService.php
@@ -0,0 +1,28 @@
+<?php
+namespace anitop\utils;
+
+class EncodingService {
+    public static function encode(string $str = '') {
+        return \mb_convert_encoding($str, 'UTF-8', 'ISO-8859-1');
+    }
+
+    public static function encodeArray($data = array()) {
+        $encoded = array();
+
+        foreach ($data as $key=>$value) {
+            $encoded[$key] = EncodingService::encode($value);
+        }
+
+        return $encoded;
+    }
+
+    public static function encodeMultipleArray($data = array()) {
+        $encoded = array();
+
+        foreach ($data as $value) {
+            $encoded[] = EncodingService::encodeArray($value);
+        }
+
+        return $encoded;
+    }
+}
