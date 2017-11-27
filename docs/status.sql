70624faa8f8aea4c8af74e867a980cd126ed1970
Calvin Gerling <calvingerling@outlook.com>
2017-11-27 08:53:57 -0200

    Add initial status load

---
+++ docs/status.sql
@@ -0,0 +1,3 @@
+INSERT INTO status (statusid, name, description) VALUES (1, 'Active', 'Currently watching');
+INSERT INTO status (statusid, name, description) VALUES (2, 'Inactive', 'Watched but not finished');
+INSERT INTO status (statusid, name, description) VALUES (3, 'Finished', 'Watched every episode');
