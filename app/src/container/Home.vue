6a6a99c6b0f38bf9c73d04676c5cfab1f981c136
Calvin Gerling <calvingerling@outlook.com>
2017-11-28 04:43:59 -0200

    Add pagination to Home

--- app/src/container/Home.vue
+++ app/src/container/Home.vue
@@ -1,23 +1,42 @@
 <template>
-  <anime-list :animes="animes" />
+  <div>
+    <pagination @changePage="changePage" :size="animes.length" :total="total">
+      <anime-list :animes="animes" />
+    </pagination>
+  </div>
 </template>
 <script>
 import AnimeList from '@/components/AnimeList'
+import Pagination from '@/components/Pagination'
 import * as animeService from '@/services/animeService'
 
 export default {
   name: 'Home',
-  components: { AnimeList },
+  components: { AnimeList, Pagination },
   data () {
     return {
-      animes: []
+      animes: [],
+      total: 0,
+      loading: false
     }
   },
   methods: {
-    obtainAnime () {
-      animeService.obtainAll().then(({ data }) => {
+    changePage ({ page, size }) {
+      this.obtainAnime(page, size)
+    },
+    obtainAnime (page, size) {
+      this.startLoading()
+      animeService.obtainAll(page, size).then(({ data }) => {
         this.animes = data.animes
-      })
+        this.total = data.total
+        this.stopLoading()
+      }).catch(this.stopLoading)
+    },
+    startLoading () {
+      this.loading = true
+    },
+    stopLoading () {
+      this.loading = false
     }
   },
   created () {
